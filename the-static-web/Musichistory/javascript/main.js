e2bfd7f463fc7b97f10a1c08d7562af27865cc0c
Adam Myers <Tavern@adams-mac.attlocal.net>
2017-01-19 21:21:39 -0600

    Added an 'add song' and 'remove song' button

--- the-static-web/Musichistory/javascript/main.js
+++ the-static-web/Musichistory/javascript/main.js
@@ -1,4 +1,6 @@
 var mercyButton = document.getElementById("mercy-button");
+var addButton = document.getElementById("add-song-button");
+var removeButton = document.getElementById("remove-song-button");
 
 var songs = [];
 songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
@@ -57,10 +59,8 @@ for (i = 0; i < songs.length; i++) {
 
 
 function createFrameWork() {
-
 	var Joined = [];
-
-	for (i = 0; i < songs.length; i++) {
+	for (i = 0; i < pulledSongName.length; i++) {
 
 		var list = `<h2>${pulledSongName[i]}</h2>
 						<ol>
@@ -77,6 +77,57 @@ function createFrameWork() {
 	document.getElementById("song-list-div").innerHTML = finished;
 }
 
+function addSong () {
+
+	var pleaseConfirm = document.getElementById("response");
+	var songInput = document.getElementById("add-song").value;
+	var artistInput = document.getElementById("add-artist").value;
+	var albumInput = document.getElementById("add-album").value;
+	var genreInput = document.getElementById("add-genre").value;
+
+		if (songInput === "") {
+			alert("You need to enter a song name");
+			return;
+		} else if (artistInput === "") {
+			alert("You need to enter an artist name");
+			return;
+		} else if (albumInput === "") {
+			alert("You need to enter an album name");
+			return;
+		} else if (genreInput === "") {
+			alert("You need to enter a genre type");
+			return;
+		}
+
+	pulledSongName.push(songInput);
+	pulledArtistName.push(artistInput);
+	pulledAlbumName.push(albumInput);
+	genres.push(genreInput);
+	clearInputs();
+	pleaseConfirm.innerHTML = "Click 'Review Songs' to add your song to the list.";
+}
+
+function removeSong() {
+	var pleaseConfirm = document.getElementById("response");
+	pulledSongName.pop();
+	pulledArtistName.pop();
+	pulledAlbumName.pop();
+	genres.pop();
+	createFrameWork();
+	pleaseConfirm.innerHTML = "The last song has been removed."
+}
+
+function clearInputs() {
+	document.getElementById("add-song").value = "";
+	document.getElementById("add-artist").value = "";
+	document.getElementById("add-album").value = "";
+	document.getElementById("add-genre").value = "";
+
+}
+
 mercyButton.addEventListener('click', createFrameWork);
+addButton.addEventListener('click', addSong);
+removeButton.addEventListener('click', removeSong);
+
 
 
